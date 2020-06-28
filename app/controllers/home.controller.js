40cae0bc2fa2f6727dee18cfa97595f54016eef6
waterfausett <waterfausett@gmail.com>
2020-06-27 20:36:15 -0500

    no longer looking for query param

--- app/controllers/home.controller.js
+++ app/controllers/home.controller.js
@@ -5,13 +5,11 @@ const triggerService = require('../services/trigger.service');
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
-    const guildId = req.query.guildId; // TODO: prolly will move this to a dropdown at somepoint
 	res.render('index', { 
         layout: 'shared/layout', 
         title: global.title, 
         activeTabClass: '.nav-link.home',
-        user: req.user,
-        data: guildId ? await triggerService.getRows(guildId) : []
+        user: req.user
     });
 });
 
