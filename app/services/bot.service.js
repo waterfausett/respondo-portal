f033ebee2137179968335c3982094bbd94481725
waterfausett <waterfausett@gmail.com>
2020-07-05 12:57:15 -0500

    now using discord.js

--- app/services/bot.service.js
+++ app/services/bot.service.js
@@ -1,7 +1,7 @@
 const optionalRequire = require("optional-require")(require);
 const authConfig = optionalRequire('../../config/auth.config.json');
 // Required dependencies
-const Discord = require('discord.io');
+const Discord = require('discord.js');
 const NodeCache = require('node-cache');
 const botCache = new NodeCache({ stdTTL: process.env.bot_server_cache_timeout || 60 * 60 }); // default cache ttl to 1 hour
 
@@ -15,15 +15,18 @@ module.exports = {
         }
     
         const botServerIds = await new Promise((resolve, reject) => {
-            var bot = new Discord.Client({
-                token: process.env.auth_token || authConfig.auth_token,
-                autorun: true
+            const bot = new Discord.Client();
+
+            bot.once('ready', () => {
+                resolve([...bot.guilds.cache.keys()]);
+                bot.destroy();
             });
-            bot.on('ready', (evt) => {
-                resolve(Object.keys(bot.servers));
-                bot.disconnect();
-            });
+            
+            bot.on('error', (err) => logger.error(err));
+            
+            bot.login(process.env.auth_token || authConfig.auth_token);
         });
+
         botCache.set(key, botServerIds);
     
         return botServerIds;
